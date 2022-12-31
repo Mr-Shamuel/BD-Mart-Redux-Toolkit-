5b061f644a3e5bd99caa0c03bd9152d01a2d2b24
Mr-Shamuel <shamuelsam300@gmail.com>
2022-12-31 14:21:46 +0600

    updating

---
+++ src/Firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCaH7nq_hYtvjJq6RcJQPPoGCICefZ2sRc",
+    authDomain: "bd-mart-redux.firebaseapp.com",
+    projectId: "bd-mart-redux",
+    storageBucket: "bd-mart-redux.appspot.com",
+    messagingSenderId: "259852189443",
+    appId: "1:259852189443:web:4e04f71d0fbd91130d7336"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig); 
